a42f80fb1f598ef20131fb55926afbbd430b6859
KailerS <KailerS@users.noreply.github.com>
2019-11-05 19:48:21 -0700

    Added the character pictures

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
     <>
       <NavBar />
       <Header />
-      {/* <MainContent /> */}
+      <MainContent />
       {/* <Footer /> */}
     
     
--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -2,14 +2,28 @@ import React, { Component } from "react";
 
 class MainContent extends Component {
     state = {
-        beenClicked: false
+        beenClicked: false,
+        images: []
     }
 
     render () {
         return (
-            <div className="container">
-
-            </div>
+            <main className="container">
+                <div role="image" aria-label="click item" className="click-item" style={{backgroundImage: 'url("./assets/images/11.jpg")'}}></div>
+                <div role="image" aria-label="click item" className="click-item" style={{backgroundImage: 'url("./assets/images/bob.jpg")'}}></div>
+                <div role="image" aria-label="click item" className="click-item" style={{backgroundImage: 'url("./assets/images/demodog.jpeg")'}}></div>
+                <div role="image" aria-label="click item" className="click-item" style={{backgroundImage: 'url("./assets/images/dustin.jpg")'}}></div>
+                <div role="image" aria-label="click item" className="click-item" style={{backgroundImage: 'url("./assets/images/hopper.jpeg")'}}></div>
+                <div role="image" aria-label="click item" className="click-item" style={{backgroundImage: 'url("./assets/images/lucas.png")'}}></div>
+                <div role="image" aria-label="click item" className="click-item" style={{backgroundImage: 'url("./assets/images/max.jpg")'}}></div>
+                <div role="image" aria-label="click item" className="click-item" style={{backgroundImage: 'url("./assets/images/mike.jpg")'}}></div>
+                <div role="image" aria-label="click item" className="click-item" style={{backgroundImage: 'url("./assets/images/nancy.jpg")'}}></div>
+                <div role="image" aria-label="click item" className="click-item" style={{backgroundImage: 'url("./assets/images/steve.jpg")'}}></div>
+                <div role="image" aria-label="click item" className="click-item" style={{backgroundImage: 'url("./assets/images/will.jpg")'}}></div>
+                <div role="image" aria-label="click item" className="click-item" style={{backgroundImage: 'url("./assets/images/robin.jpg")'}}></div>
+            </main>
         )
     }
 }
+
+export default MainContent
