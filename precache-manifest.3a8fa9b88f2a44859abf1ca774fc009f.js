2b92ce819c493c98b16f91f8c127b3c80acab5ca
KailerS <KailerS@users.noreply.github.com>
2019-11-07 08:49:34 -0700

    Updates

---
+++ precache-manifest.3a8fa9b88f2a44859abf1ca774fc009f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe1af73afd43c93f502e5b42b9a316d9",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "f5035ffb46c47ee76f8e",
+    "url": "/clicky-game/static/js/2.8fa57949.chunk.js"
+  },
+  {
+    "revision": "acad5f2e4cfce2327743",
+    "url": "/clicky-game/static/js/main.b7e8823e.chunk.js"
+  },
+  {
+    "revision": "0f0a7c239744c2652e8a",
+    "url": "/clicky-game/static/js/runtime-main.1df8f772.js"
+  }
+]);
